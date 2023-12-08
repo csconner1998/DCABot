5dcd79529d1088d245cc939befb280204169a71a
Conner Soule <csconner1998@gmail.com>
2023-12-08 13:45:21 -0700

    whats working now

--- DCAApp/forms.py
+++ DCAApp/forms.py
@@ -1,7 +1,20 @@
 from django import forms
-from .models import InputData
+from .models import DCASettings
 
-class InputDataForm(forms.ModelForm):
+class DCASettingsCreateForm(forms.ModelForm):
     class Meta:
-        model = InputData
-        fields = '__all__'  # You can specify fields manually here as a list if needed
+        model = DCASettings
+        exclude = ['user_id', 'running', 'user']  # Exclude 'user' field from the form
+        
+
+class DCASettingsEditForm(forms.ModelForm):
+    class Meta:
+        model = DCASettings
+        exclude = ['user_id', 'running', 'user']  # Exclude 'user' field from the form
+        
+    def __init__(self, *args, **kwargs):
+        super(DCASettingsEditForm, self).__init__(*args, **kwargs)
+        # If the setting is running, disable the form fields
+        if self.instance.running:
+            for field in self.fields:
+                self.fields[field].disabled = True
--- DCAProject/urls.py
+++ DCAProject/urls.py
@@ -23,7 +23,7 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("DCAApp.urls"), name='landing_page'),
+    path('', include("DCAApp.urls"), name='dca_settings_create'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
