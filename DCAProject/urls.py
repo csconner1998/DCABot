34c0031688cfd15344f38f83b032d28f5a35ce15
Conner Soule <csconner1998@gmail.com>
2023-12-12 12:26:10 -0700

    forgot 2 files

--- DCAProject/urls.py
+++ DCAProject/urls.py
@@ -24,6 +24,5 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('', include("DCAApp.urls"), name='dca_settings_create'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
